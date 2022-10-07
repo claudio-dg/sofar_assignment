a15d3dce7aeb4af3cdb5eb012f26b6d708933051
davidebruzzo <62437632+davidebruzzo@users.noreply.github.com>
2022-10-07 17:41:26 +0200

    Update color_detection_server.py
    Added comments and cleaned the code

--- scripts/color_detection_server.py
+++ scripts/color_detection_server.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python2
 
-# Python code for Multiple Color Detection
+
+"""
+- Module: color_detection_server.py
+- Authors: D. Bruzzo, C. Del Gaizo, P. Saade, C. Tsague
+- 
+- Subscribes to:
+-		"/xtion/rgb/image_raw" : to take Tiago's camera image
+- Server:
+-	"/color_detect_service"
+-
+-
+- Description:
+-
+- This node function as a server that the main.cpp node calls it when the conveyor is stopped and before the final movement of Tiago after it grasped the object.
+- This node also updates continuously the image from image_raw to have the latest one on which we perform the color detection. 
+- Once recognised it returns the color to main.cpp, which is now capable to tell Tiago in which bin put the box. We set 1 as GREEN and 0 as BLUE. 
+-
+-
+"""
+
+
 import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
@@ -9,18 +29,20 @@ from sensor_msgs.msg import Image
 from sofar_assignment.srv import DetectColor
 import time
 
-# Capturing video through webcam
-#webcam = cv2.VideoCapture(0)
-
+# variable declaration
 img = None
 bridge = CvBridge()
+
+#subscriber initialization
 sub = rospy.Subscriber("/xtion/rgb/image_raw", Image )
 
-# Start a while loop
 def callbackFunc(req):
 	
-	# Reading the video from the
-	# webcam in image frames
+	""" 
+    Server callback executed when the main.cpp calls the color_detection_server client. 
+    This function starts the color detection routine.
+    
+    """
 	global img
 	
 
@@ -29,43 +51,24 @@ def callbackFunc(req):
 	except CvBridgeError as e:
 		rospy.logerr("Cv bridge error: {0}".format(e))
 	
-	"""
-	cv2.namedWindow("Image window",1)
-	cv2.imshow("Image window", imageFrame)
-	cv2.waitKey(10)
-	"""
-
-	#_, imageFrame = img_msg.header
-	# rospy.loginfo(img_msg.header)
-	# Convert the imageFrame in
-	# BGR(RGB color space) to
-	# HSV(hue-saturation-value)
-	# color space
-	
+	# Convert the imageFrame in BGR(RGB color space) to HSV(hue-saturation-value) color space	
 	hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
 
 	
-	# Set range for blue color and
-	# define mask
+	# Set range for blue color and define mask
 	blue_lower = np.array([94, 80, 2], np.uint8)
 	blue_upper = np.array([120, 255, 255], np.uint8)
 	blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-	#cv2.imshow('image window name', imageFrame)
-	#cv2.imshow('mask window name', blue_mask)
-	#cv2.waitKey(10)
+	
 
 
-	# Set range for green color and
-	# define mask
+	# Set range for green color and define mask
 	green_lower = np.array([25, 52, 72], np.uint8)
 	green_upper = np.array([102, 255, 255], np.uint8)
 	green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 	
 
-	# Morphological Transform, Dilation
-	# for each color and bitwise_and operator
-	# between imageFrame and mask determines
-	# to detect only that particular color
+	# Morphological Transform, Dilation for each color and bitwise_and operator between imageFrame and mask determines to detect only that particular color
 	kernal = np.ones((5, 5), "uint8")
 
 	# For blue color
@@ -77,9 +80,6 @@ def callbackFunc(req):
 	green_mask = cv2.dilate(green_mask, kernal)
 	res_green = cv2.bitwise_and(imageFrame, imageFrame,
 								mask = green_mask)
-	
-	
-
 	
 	# Creating contour to track green color
 	contours, hierarchy = cv2.findContours(green_mask,
@@ -99,9 +99,8 @@ def callbackFunc(req):
 						1.0, (0, 255, 0))
 			# Program Termination
 			cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
-			#cv2.waitKey(30)
-			#time.sleep(5)
-			if cv2.waitKey(10) & 0xFF == ord('q'): ################## I BELIEVE THAT being called once it doesnt manage to show the image, if we don t solve, cancel this part of the code showing rectangles
+			
+			if cv2.waitKey(10) & 0xFF == ord('q'): 
 				cap.release()
 				cv2.destroyAllWindows()
 
@@ -133,12 +132,22 @@ def callbackFunc(req):
 	
 
 def imageCallback(img_msg):
+	
+	""" 
+    Callback that updates the image from Tiago's camera.
+    
+    """
 	global img
 	img = img_msg
 		
 
 def setup_colorDetect_server():
-    
+	
+	""" 
+    Function called to setup the server and also subscribe to the /xtion/rgb/image_raw topic
+	
+    """
+    # initialize the node
 	rospy.init_node("color_detect_server_node")
 	rospy.Subscriber("/xtion/rgb/image_raw", Image,  imageCallback)
 	rospy.Service("color_detect_service", DetectColor, callbackFunc)
@@ -149,7 +158,6 @@ def setup_colorDetect_server():
 if __name__=='__main__':
 
 	setup_colorDetect_server()
-    #rospy.init_node("color_detection_node")
 
     
 
